5a829e759f8575b89692d10c19fc25e989cc0760
Diego BustamantediegoBustamante <diegi.b58@gmail.com>
2023-06-21 19:06:40 -0300

    add index to DB

--- src/DAO/models/products.model.js
+++ src/DAO/models/products.model.js
@@ -3,13 +3,13 @@ import { Schema, model } from "mongoose";
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const schema = new Schema({
-    title: { type: String, required: true, max: 100 },
+    title: { type: String, required: true, max: 100, index: true },
     description: { type: String, required: true, max: 100 },
     price: { type: Number, required: true},
     thumbnail: { type: String, required: false, max: 100 },
     code: { type: String, required: true, max: 100 },
     stock: { type: Number, required: true},
-    category: { type: String, required: true, max: 100 },
+    category: { type: String, required: true, max: 100, index: true },
     status: { type: Boolean, required: true, max: 100},
 });
 
